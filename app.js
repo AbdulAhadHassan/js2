a4bc12ec3704628e5ac7d72473e9cb87766409f4
Abdul Ahad Hassan <malikabdulahadhassan@gmail.com>
2024-03-02 21:25:51 +0500

    java script assignment 2

---
+++ app.js
@@ -0,0 +1,60 @@
+
+
+// chapter 4
+
+
+
+// Q1
+// var sent1 = 'sentence 1'; var sent2 = 'sentence 2';var sent3 = 'sentence 3';
+
+
+// Q2
+// var let = 'variable 1'
+// var string = 'variable 2'
+// var alert = 'variable 3'
+// var docment = 'variable 4'
+// var promtp = 'variable 5'
+
+
+// var variableNumberOne = 'variable 6'
+// var message_1 = 'variable 7'
+// var headerSlider = 'variable 8'
+// var navbar_bars = 'variable 9'
+// var videoPlayer = 'variable 10'
+
+
+// Q3
+
+// a
+// var mainHeading = 'Rules for naming JS variables';
+// document.write("<h1>" , mainHeading , "</h1>")
+
+// b
+// document.write("<p> Variable names can only contain letters, numbers, $ and _. For example $my_1stVariable </p>" )
+
+// c
+// document.write("<p> Variables must begin with a letters, $ or _. For example $name, _name or name </p>")
+
+// d
+// document.write("<p> Variable names are case sensitive </p>")
+
+// e
+// document.write("<p> Variable names should not be JS keywodeords </p>")
+
+
+
+// chapter 2
+
+// Q1
+var firstNumber = +prompt('enter first number')
+var secondNumber = +prompt('enter second number')
+var totalNumber = firstNumber + secondNumber
+document.write('sum of ',firstNumber,' and ',secondNumber,' is ',totalNumber)
+
+
+
+
+
+
+
+
