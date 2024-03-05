71b5ad3c7e8102a18d01b6ad8c7ab4cab7cf0927
Abdul Ahad Hassan <malikabdulahadhassan@gmail.com>
2024-03-05 21:32:22 +0500

    js2

--- app.js
+++ app.js
@@ -43,18 +43,162 @@
 
 
 
-// chapter 2
+
+// chapter 5
+
+
 
 // Q1
-var firstNumber = +prompt('enter first number')
-var secondNumber = +prompt('enter second number')
-var totalNumber = firstNumber + secondNumber
-document.write('sum of ',firstNumber,' and ',secondNumber,' is ',totalNumber)
+// var firstNumber = +prompt('enter first number')
+// var secondNumber = +prompt('enter second number')
+// var totalNumber = firstNumber + secondNumber
+// document.write('sum of ',firstNumber,' and ',secondNumber,' is ',totalNumber)
+
+// Q2
+// var firstNumber = +prompt('enter first number')
+// var secondNumber = +prompt('enter second number')
+// var totalNumber = firstNumber - secondNumber
+// document.write('subtraction of ',firstNumber,' and ',secondNumber,' is ',totalNumber)
+
+// var firstNumber = +prompt('enter first number')
+// var secondNumber = +prompt('enter second number')
+// var totalNumber = firstNumber / secondNumber
+// document.write('division of ',firstNumber,' and ',secondNumber,' is ',totalNumber)
+
+// var firstNumber = +prompt('enter first number')
+// var secondNumber = +prompt('enter second number')
+// var totalNumber = firstNumber * secondNumber
+// document.write('multiplication of ',firstNumber,' and ',secondNumber,' is ',totalNumber)
+
+// var firstNumber = +prompt('enter first number')
+// var secondNumber = +prompt('enter second number')
+// var totalNumber = firstNumber % secondNumber
+// document.write('modulus of ',firstNumber,' and ',secondNumber,' is ',totalNumber)
+
+
+// Q3
+// var userNumber = +prompt('enter ytou number')
+// document.write('Value after variable declaration is: ', userNumber)
+// document.write('<P>','Initial value: ', userNumber, '</P>')
+// var userNumber = userNumber + 1
+// document.write('<P>','Value after increment is: ', userNumber, '</P>')
+// var userNumber = userNumber + 7
+// document.write('<P>','Value after addition is: ', userNumber, '</P>')
+// var userNumber = userNumber - 1
+// document.write('<P>','Value after decrement is: ', userNumber, '</P>')
+// var userNumber = userNumber / 3
+// document.write('<P>','The remainder is: ', userNumber, '</P>')
+
+
+// Q4
+// var ticketCost = 600 ;
+// var ticketNumber = +prompt('write the Numbers of tickets')
+// var totalCost = ticketNumber * ticketCost
+// document.write('<h1>' , 'Total cost to buy ', ticketNumber, ' ticket to the movies is ', totalCost ,' PKR'  )
+
+// Q5
+// var userNumber = +prompt('Table of')
+// document.write('<h1>','Table of  ', userNumber, '</h1>')
+// document.write('<h1>',userNumber,' X 1 = ', userNumber, '</h1>')
+// var userNumberTotal = 2 * userNumber
+// document.write('<h1>',userNumber,' X 2 = ', userNumberTotal, '</h1>')
+// var userNumberTotal = 3 * userNumber
+// document.write('<h1>',userNumber,' X 3 = ', userNumberTotal, '</h1>')
+// var userNumberTotal = 4 * userNumber
+// document.write('<h1>',userNumber,' X 4 = ', userNumberTotal, '</h1>')
+// var userNumberTotal = 5 * userNumber
+// document.write('<h1>',userNumber,' X 5 = ', userNumberTotal, '</h1>')
+// var userNumberTotal = 6 * userNumber
+// document.write('<h1>',userNumber,' X 6 = ', userNumberTotal, '</h1>')
+// var userNumberTotal = 7 * userNumber
+// document.write('<h1>',userNumber,' X 7 = ', userNumberTotal, '</h1>')
+// var userNumberTotal = 8 * userNumber
+// document.write('<h1>',userNumber,' X 8 = ', userNumberTotal, '</h1>')
+// var userNumberTotal = 9 * userNumber
+// document.write('<h1>',userNumber,' X 9 = ', userNumberTotal, '</h1>')
+// var userNumberTotal = 10 * userNumber
+// document.write('<h1>',userNumber,' X 10 = ', userNumberTotal, '</h1>')
 
 
 
+// Q6
+// var userValue1 = +prompt('Enter °C value')
+// var c = (userValue1 - 32 ) * 5/9
+// document.write('<P>',userValue1 ,'°C is ', c , '°F' , '</P>')
+// var userValue2 = +prompt('Enter °F value')
+// var f = (userValue2 * 5/9) + 32
+// document.write('<P>',userValue1 ,'°F is ', f , '°C' , '</P>')
 
 
+// Q7
+// var item1 = 650 
+// document.write('<p>','Orice of item 1 is ', item1 ,'</p>' )
+// var quantity1  = +prompt('Quantity of item 1 ')
+// document.write('<p>','Quantity nif item 1 is ', quantity1 ,'</p>')
+// var item1Cost = item1 * quantity1
+// var item2 = 120 
+// document.write('<p>','Orice of item 2 is ', item2 ,'</p>')
+// var quantity2  = +prompt('Quantity of item 2 ')
+// document.write('<p>','Quantity nif item 1 is ', quantity2 ,'</p>')
+// var item2Cost = item2 * quantity2
+// var tax = 100
+// document.write('<p>','Shopping fees ', tax ,'</p>')
+// document.write('<p>','Total cost ', tax + item1Cost + item2Cost ,'</p>')
 
 
+// Q8
+// var totalMarks = +prompt('Enter full marks')
+// var userMarks = +prompt('Enter your marks')
+// var percent = (userMarks / totalMarks ) * 100
+// document.write('<p>','Total marks is ', totalMarks ,'</p>')
+// document.write('<p>','Your marks is ', userMarks ,'</p>')
+// document.write('<p>','Your percentage is ', percent , '%' , '</p>')
 
+
+// Q9
+// var user = + prompt('Enter amount of Dollar')
+// var usd = 104.80 * user
+// var user1 = + prompt('Enter amount of Riyal')
+// var riyal = 27 * user1
+ 
+// document.write( '<h1>', 'Curency in PRK' ,'</h1>')
+// document.write( '<p>', 'Total Curency in from dollar to rupees ' , usd  ,'PRK','</p>')
+// document.write( '<p>', 'Total Curency in from riyal to rupees ' , riyal  ,'PRK','</p>')
+ 
+
+
+// Q10
+// var userVariable =+prompt('write number')
+// var finalAnswer = ((userVariable + 5 )*10)/2
+
+
+// Q11
+// document.write('<h1>', 'Age calculator' ,'</h1>')
+// var currentYear = +prompt('Enter current year')
+// var birthYear = +prompt('Enter birth year')
+// var age = currentYear-birthYear
+// var age1 = currentYear-birthYear-1
+// document.write('your age is ', age, ' or ' , age1)
+
+// Q12
+// var radius = +prompt('Raduis if the circle')
+// var circumference = 2 * 3.142 * radius
+// var area = (radius**2)*3.142
+// document.write('<h1>', 'The geometrizer' ,'</h1>')
+// document.write('<p>', 'The Radius of the circle is ' , radius ,'</p>')
+// document.write('<p>', 'The circumference  of the circle is ' , circumference ,'</p>')
+// document.write('<p>', 'The Area of the circle is ' , area ,'</p>')
+
+
+// Q13
+// var snack = prompt('your favorate snack is ')
+// var age = +prompt('Your cuurent age')
+// var maxage = +prompt('your max age')
+// var snackamount = +prompt('your amount of snak per day')
+// document.write('<h1>', 'The lifetime supply calculator' ,'</h1>')
+// document.write('<p>', 'Fav snack' , snack ,'</p>')
+// document.write('<p>', 'Current age ' , age ,'</p>')
+// document.write('<p>', 'Estimated max age ' , maxage ,'</p>')
+// document.write('<p>', 'Amount of snack per day ' , snack ,'</p>')
+// var result = ((maxage-age)*365)*snackamount
+// document.write('<p>', 'You will need ' , result,' ', snack,' to last ypu until you rip of olf age of ', maxage  ,'</p>')
